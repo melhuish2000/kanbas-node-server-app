0abeaf167e4d93d24a0755a65572caa4ac09cea2
melhuish2000 <melhuish.a@northeastern.edu>
2023-12-04 23:20:54 -0500

    done

--- app.js
+++ app.js
@@ -3,11 +3,42 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import Hello from "./hello.js";
-const app = express();
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
-app.use(cors());
+
+const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+   );
+
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
+UserRoutes(app);
+
 ModuleRoutes(app);
 Hello(app);
 CourseRoutes(app);
